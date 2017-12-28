c608c66408ec21f8d69d53ebb151c2c8ea9df38c
Thomas Sin <sin@Thomass-MacBook-Pro.local>
2017-12-28 18:10:51 -0500

    Added StringToCharArrayUsingStrcpy to C++

---
+++ C++/StringToCharArrayUsingStrcpy/StringToCharArrayUsingStrcpy/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  StringToCharArrayUsingStrcpy
+//
+//  Created by Thomas Sin on 2017-12-28.
+//  Copyright © 2017 TomSin. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+int main(int argc, const char * argv[]) {
+    std::string str = "Mr Johnson is Nice";
+    
+    char charArray[str.size()];
+    strcpy(charArray, str.c_str());
+    
+    for(char* it = charArray ; *it ; ++it)
+    {
+        std::cout<<*it<<std::endl;
+    }
+    
+}
