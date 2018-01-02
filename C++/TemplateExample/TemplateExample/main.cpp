803863c27f54981d66d2c3882af63fbf8464a5e0
Thomas Sin <sin@Thomass-MacBook-Pro.local>
2018-01-02 11:22:19 -0500

    Added TemplateExample to C++

--- C++/TemplateExample/TemplateExample/main.cpp
+++ C++/TemplateExample/TemplateExample/main.cpp
@@ -7,22 +7,25 @@
 //
 
 #include <iostream>
+
 template<class T>
-T GetMax(T a, T b)
+T GetSum(T a, T b)
 {
-    T result = a>b ? a:b;
+    T result = a + b;
     return result;
 }
 
 int main(int argc, const char * argv[]) {
     int i = 3, j = 5, k;
-    float l = 10.0, m = 17.0, n;
+    float l = 10.4, m = 17.8, n;
     
-    k = GetMax<int>(i, j); // expected outcome: k = 5
+    k = GetSum<int>(i, j); // expected outcome: k = 8
     std::cout << k << std::endl;
     
-    n = GetMax<float>(l, m); // expected outcome: n = 17.0
+    n = GetSum<float>(l, m); // expected outcome: n = 28.2
     std::cout << n << std::endl;
     
+    n = GetSum<int>(l, m); // expected outcome: n = 27; (cuz floats are inputted as int)
+    std::cout << n << std::endl;
     return 0;
 }
