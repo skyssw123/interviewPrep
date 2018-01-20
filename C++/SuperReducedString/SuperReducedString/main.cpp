e86917b2e46dec7ce8dc111e0daf81914e321f50
Thomas Sin <sin@Thomass-MacBook-Pro.local>
2018-01-20 17:23:57 -0500

    Added SuperReducedString to C++

---
+++ C++/SuperReducedString/SuperReducedString/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  SuperReducedString
+//
+//  Created by Thomas Sin on 2018-01-20.
+//  Copyright © 2018 TomSin. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    int i=0;
+    
+    
+    while(i < static_cast<int>(str.size()-1)) {
+        if(i>-1 && str[i] == str[i+1]) {
+            str.erase(i,2);
+            i--;
+        } else
+            i++;
+    }
+    
+    if(str.empty())
+        cout << "Empty String" << endl;
+    else
+        cout << str << endl;
+    return 0;
+}
